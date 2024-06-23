f5db5e541df815127c3390785486b033d5679ce9
coesu <something@gmail.com>
2024-06-23 20:18:12 +0200

    lock

--- hosts/aion/default.nix
+++ hosts/aion/default.nix
@@ -13,7 +13,7 @@
   programs.steam.enable = true;
   programs.steam.gamescopeSession.enable = true;
   programs.gamemode.enable = true;
-  environment.systemPackages = with pkgs; [ protonup ];
+  environment.systemPackages = with pkgs; [protonup];
   environment.sessionVariables = {
     STEAM_EXTRA_COMPAT_TOOLS_PATHS = "\${HOME}/.steam/root/compatibilitytools.d";
   };
