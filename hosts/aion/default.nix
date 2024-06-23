91c23db3ac676258a396c6a1a079b05268250cd9
coesu <something@gmail.com>
2024-06-23 13:37:50 +0200

    steam

--- hosts/aion/default.nix
+++ hosts/aion/default.nix
@@ -9,4 +9,12 @@
     ./hardware-configuration.nix
   ];
   networking.hostName = "aion";
+
+  programs.steam.enable = true;
+  programs.steam.gamescopeSession.enable = true;
+  programs.gamemode.enable = true;
+  environment.systemPackages = with pkgs; [ protonup ];
+  environment.sessionVariables = {
+    STEAM_EXTRA_COMPAT_TOOLS_PATHS = "\${HOME}/.steam/root/compatibilitytools.d";
+  };
 }
