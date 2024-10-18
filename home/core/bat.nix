a840856886dc9b60ada5cce73153a4f6fba19c3c
coesu <something@gmail.com>
2024-10-18 10:07:58 +0200

    updates

--- home/core/bat.nix
+++ home/core/bat.nix
@@ -11,8 +11,7 @@
       inherit
         (pkgs.bat-extras)
         batgrep # search through and highlight files using ripgrep
-        # batdiff # Diff a file against the current git index, or display the diff between to files
-
+        batdiff # Diff a file against the current git index, or display the diff between to files
         batman
         ; # read manpages using bat as the formatter
     };
