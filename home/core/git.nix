0467199d74f8bd1c0055643b564f8cad3ae7c2a0
coesu <something@gmail.com>
2024-10-27 21:10:49 +0100

    updates

--- home/core/git.nix
+++ home/core/git.nix
@@ -9,25 +9,11 @@
     package = pkgs.gitAndTools.gitFull;
     userName = "coesu";
     userEmail = "something@gmail.com";
-    # aliases = { };
-    # extraConfig = {
-    #   init.defaultBranch = "main";
-    #   url = {
-    #     "ssh://git@github.com" = {
-    #       insteadOf = "https://github.com";
-    #     };
-    #     "ssh://git@gitlab.com" = {
-    #       insteadOf = "https://gitlab.com";
-    #     };
-    #   };
-    #
-    #   user.signing.key = "41B7B2ECE0FAEF890343124CE8AA1A8F75B56D39";
-    #   #TODO sops - Re-enable once sops setup complete
-    #   commit.gpgSign = false;
-    #   gpg.program = "${config.programs.gpg.package}/bin/gpg2";
-    # };
-    # enable git Large File Storage: https://git-lfs.com/
-    # lfs.enable = true;
+    aliases = {};
+    extraConfig = {
+      init.defaultBranch = "main";
+      pull.ff = "only";
+    };
     ignores = [
       ".direnv"
       "result"
