5b08bce8c0784faf36b45a0c214662f7dce96373
coesu <something@gmail.com>
2024-11-03 11:57:01 +0100

    updates

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -34,6 +34,8 @@
   programs.zsh.enable = true;
 
   programs.sway.enable = true;
+  programs.sway.wrapperFeatures.gtk = true;
+
   # services.xserver.windowManager.qtile = {
   #   enable = true;
   #   extraPackages = python3Packages:
@@ -41,10 +43,10 @@
   #       qtile-extras
   #     ];
   # };
+
   programs.hyprland = {
     enable = true;
   };
-  programs.sway.wrapperFeatures.gtk = true;
 
   xdg = {
     portal = {
@@ -128,7 +130,7 @@
       "networkmanager"
       "docker"
     ];
-    shell = pkgs.zsh;
+    shell = pkgs.nushell;
     packages = with pkgs; [
       tree
       zsh
@@ -140,6 +142,7 @@
   environment.systemPackages = with pkgs; [
     protonup
     vim
+    nushell
     ffmpeg
     imagemagick
     neovim
