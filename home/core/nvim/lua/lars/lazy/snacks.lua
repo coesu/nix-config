b9bd5b95512b9956501b623d4aad0c235e41d938
coesu <something@gmail.com>
2024-12-10 21:25:11 +0100

    updates

--- home/core/nvim/lua/lars/lazy/snacks.lua
+++ home/core/nvim/lua/lars/lazy/snacks.lua
@@ -73,20 +73,6 @@ return {
 				desc = "Dismiss All Notifications",
 			},
 			{
-				"<c-/>",
-				function()
-					Snacks.terminal()
-				end,
-				desc = "Toggle Terminal",
-			},
-			{
-				"<c-_>",
-				function()
-					Snacks.terminal()
-				end,
-				desc = "which_key_ignore",
-			},
-			{
 				"]]",
 				function()
 					Snacks.words.jump(vim.v.count1)
@@ -137,16 +123,11 @@ return {
 					-- Create some toggle mappings
 					Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>us")
 					Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
-					Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>uL")
 					Snacks.toggle.diagnostics():map("<leader>ud")
 					Snacks.toggle.line_number():map("<leader>ul")
 					Snacks.toggle
 						.option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 })
 						:map("<leader>uc")
-					Snacks.toggle.treesitter():map("<leader>uT")
-					Snacks.toggle
-						.option("background", { off = "light", on = "dark", name = "Dark Background" })
-						:map("<leader>ub")
 					Snacks.toggle.inlay_hints():map("<leader>uh")
 				end,
 			})
